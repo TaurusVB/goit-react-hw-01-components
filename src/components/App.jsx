834a3b94ae4d83a24f58f8f37d0ac7e35f87a35e
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-05-21 18:30:30 +0300

    add components on my page and styles for them

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
+import user from '../responses/user.json';
+import data from '../responses/data.json';
+import friends from '../responses/friends.json';
+import transactions from '../responses/transactions.json';
+
+const { username, tag, location, avatar, stats } = user;
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory transactions={transactions} />
     </div>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,27 @@
+import css from './TransactionHistory.module.css'
+
+export default function TransactionHistory({ transactions }) {
+  return (
+    <table className={css.transactionHistory}>
+      <thead className={css.tableHead}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(item => {
+          return (
+            <tr key={item.id}>
+              <td>{item.type}</td>
+              <td>{item.amount}</td>
+              <td>{item.currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
